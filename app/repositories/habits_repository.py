5bbcd8b02ced811e7e15f7962daf9e92e3203477
edgardanielgd <edgardanielgd123@gmail.com>
2023-09-11 16:39:17 -0500

    Refactorized model data source

--- app/repositories/habits_repository.py
+++ app/repositories/habits_repository.py
@@ -1,89 +1,88 @@
 """ a class to read from postgresql database using sqlalchemy"""
-from sqlalchemy import create_engine, URL, MetaData, Table, select
-from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 import pandas as pd
 import asyncio
 import os
 import app.models.repo_models as rm
+import json
 
 load_dotenv()
-url = URL.create(
-    drivername="postgresql",
-    username=os.getenv("POSTGRES_USER"),
-    password=os.getenv("POSTGRES_PASSWORD"),
-    host=os.getenv("POSTGRES_HOST"),
-    port=os.getenv("POSTGRES_PORT"),
-    database=os.getenv("POSTGRES_DB"),
-)
+url = os.getenv("GATEWAY_URL")
+
+# TODO: Connect with actual gateway when it's implemented
 
 class HabitRepository:
     def __init__(self):
-        self.engine = create_engine(url)
-        self.session = sessionmaker(bind=self.engine)
-        self.metadata = MetaData()
+        habits_file = open('data/habits.json')
+        self.habits = json.load(habits_file)
 
-        self.cat = Table('Category', self.metadata, autoload=True)
-        self.hab = Table('Habit', self.metadata, autoload=True)
-        self.hab_rec = Table('Habit_Recurrency', self.metadata, autoload=True)
-        self.hab_data = Table('Habit_data_Collected', self.metadata, autoload=True)
+    def get_habit(self, hab_id):
+        # Query to microservice rather than on local file
+        for habit in self.habits:
+            if habit['hab_id'] == hab_id:
+                return habit
+        return None
+
+    def get_recurrence_data(self, rec_id):
+        # Query to microservice rather than on local file
+        for habit in self.habits:
+            for rec in habit['recurrences']:
+                if rec['hab_rec_id'] == rec_id:
+                    return rec['data']
+                
+        return None
+    
+    def get_habit_recurrences(self, hab_id):
+        habit = self.get_habit(hab_id)
+
+        if habit is None:
+            return None
+
+        return habit['recurrences']
     
     async def get_hab_is_yn(self, hab_id):
-        query = select([
-            self.hab.c.hab_is_yn,
-        ]).select_from(
-            self.hab
-        ).where(
-            self.hab.c.hab_id == hab_id
-        )
+        habit = self.get_habit(hab_id)
 
-        result = await asyncio.get_event_loop().run_in_executor(None, self.engine.execute, query)
-        return result.scalar()
-    
+        if habit is None:
+            return None
+
+        return habit['hab_is_yn']
+
     async def get_hab_rec(self, hab_id) -> rm.HabRec:
-        query = select([
-            self.hab_rec.c.hab_rec_id,
-            self.hab_rec.c.hab_rec_freq_type,
-            self.hab_rec.c.hab_rec_goal,
-        ]).select_from(
-            self.hab.rec
-        ).where(
-            self.hab_rec.c.hab_id == hab_id
+        
+        recurrences = self.get_habit_recurrences(hab_id)
+
+        if recurrences is None or len(recurrences) == 0:
+            return None
+        
+        recurrence = recurrences[0]
+
+        return rm.HabRec(
+            hab_rec_id=recurrence['hab_rec_id'],
+            hab_rec_freq_type=recurrence['hab_rec_freq_type'],
+            hab_rec_goal=recurrence['hab_rec_goal']
         )
 
-        result = await asyncio.get_event_loop().run_in_executor(None, self.engine.execute, query)
-        data = result.fetchone()
-
-        if data is not None:
-            return rm.HabRec(
-                hab_rec_id=data[0],
-                hab_rec_freq_type=data[1],
-                hab_rec_goal=data[2],
-            )
-        return data
-        
     async def get_habit_data(self, hab_id) -> rm.HabData:
-        #is_yn = await self.get_hab_is_yn(hab_id)
         hab_rec = await self.get_hab_rec(hab_id)
-        
+
         if hab_rec is None:
             return None
         
-        query = select([
-            self.hab_data.c.hab_dat_id,
-            self.hab_data.c.hab_dat_amount,
-            self.hab_data.c.hab_dat_collected_at,
-        ]).select_from(
-            self.hab_data
-        ).where(
-            self.hab.c.hab_rec_id == hab_rec[0]
-        ).order_by(self.hab_data.c.hab_dat_collected_at.desc())
+        data = self.get_recurrence_data(hab_rec.hab_rec_id)
 
-        response = await asyncio.get_event_loop().run_in_executor(None, pd.read_sql, query, self.engine)
-        if response.empty:
+        if data is None:
             return None
-        response['hab_dat_collected_at'] = pd.to_datetime(response['hab_dat_collected_at'])
-        response[['year', 'week', 'weekday']] = response['hab_dat_collected_at'].apply(lambda x: pd.Series(x.isocalendar()))
-        response['month'] = response['hab_dat_collected_at'].apply(lambda x: x.month)
-
-        return rm.HabData(hab_rec=hab_rec, data=response)
+        
+        # Generate data
+        data = pd.json_normalize(data)
+        
+        data['hab_dat_collected_at'] = pd.to_datetime(data['hab_dat_collected_at'])
+        data[['year', 'week', 'weekday']] = data['hab_dat_collected_at'].apply(lambda x: pd.Series(x.isocalendar()))
+        data['month'] = data['hab_dat_collected_at'].apply(lambda x: x.month)
+        
+        return rm.HabData(
+            hab_rec=hab_rec,
+            data=data
+        )
+        
