34d8cca06e5880eb562856ca289b9a915d31a495
maherreramu <64550002+maherreramu@users.noreply.github.com>
2023-09-17 03:47:41 -0500

    Add exception handlers for http exceptions status codes

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, status, Request
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
+from app.exceptions.exceptions import AppConnectionError, AppDatabaseError, HabitNotFoundError
 from app.routers import report_router
 import uvicorn
 
@@ -19,6 +21,27 @@ app.add_middleware(
     allow_methods=["GET"],
     allow_headers=["*"],
 )"""
+
+@app.exception_handler(AppConnectionError)
+async def connection_error_handler(request: Request, exc: AppConnectionError):
+    return JSONResponse(
+        status_code=status.HTTP_503_SERVICE_UNAVAILABLE,
+        content={"message": str(exc)},
+    )
+
+@app.exception_handler(AppDatabaseError)
+async def database_error_handler(request: Request, exc: AppDatabaseError):
+    return JSONResponse(
+        status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        content={"message": str(exc)},
+    )
+
+@app.exception_handler(HabitNotFoundError)
+async def habit_not_found_error_handler(request: Request, exc: HabitNotFoundError):
+    return JSONResponse(
+        status_code=status.HTTP_404_NOT_FOUND,
+        content={"message": str(exc)},
+    )
 
 app.include_router(report_router.router, prefix="/api/stats", tags=["statistics"])
 
