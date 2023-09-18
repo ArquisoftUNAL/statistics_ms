d28603c02c165ba517a7f365293f9756f6f2bf17
maherreramu <64550002+maherreramu@users.noreply.github.com>
2023-09-17 23:43:53 -0500

    Update 'hab_id' type to UUID type and delete endpoint_arguments_model

--- app/models/endpoint_arguments_model.py
+++
@@ -1,4 +0,0 @@
-from pydantic import BaseModel, Field
-
-class IdModel(BaseModel):
-    id: str = Field(pattern=r'^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$')
--- app/routers/report_router.py
+++ app/routers/report_router.py
@@ -1,16 +1,16 @@
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends
 from app.models import report_models as md
-from app.models.endpoint_arguments_model import IdModel
 from app.services.habit_report import HabitReport
 from app.repositories.habits_repository import HabitRepository
 from sqlalchemy import  URL
+from uuid import UUID
 from sqlalchemy.ext.asyncio import create_async_engine
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 url = URL.create(
-    drivername="postgresql",
+    drivername="postgresql+asyncpg",
     username=os.getenv("POSTGRES_USER"),
     password=os.getenv("POSTGRES_PASSWORD"),
     host=os.getenv("POSTGRES_HOST"),
@@ -28,34 +28,34 @@ def get_hs():
 router = APIRouter()
 
 @router.get("/yn_report/{hab_id}", response_model=md.HabitYNResumeReportModel)
-async def get_habit_yn_report(hab_id: IdModel, hs: HabitReport = Depends(get_hs)):
+async def get_habit_yn_report(hab_id: UUID, hs: HabitReport = Depends(get_hs)):
     return await hs.get_habit_yn_report(hab_id)
 
 @router.get("/measure_report/{hab_id}", response_model=md.HabitMeasureReportModel)
-async def get_habit_measure_report(hab_id: IdModel, hs: HabitReport = Depends(get_hs)):
+async def get_habit_measure_report(hab_id: UUID, hs: HabitReport = Depends(get_hs)):
     return await hs.get_habit_measure_report(hab_id)
 
 @router.get("/measure/resume/{hab_id}", response_model=md.HabitMeasureResumeReportModel)
-async def get_habit_measure_resume(hab_id: IdModel, hs: HabitReport = Depends(get_hs)):
+async def get_habit_measure_resume(hab_id: UUID, hs: HabitReport = Depends(get_hs)):
     body = hs.get_habit_measure_resume(hab_id)
     return body
 
 @router.get("/measure/history/{hab_id}", response_model=md.HabitMeasureHistoryReportModel)
-async def get_habit_measure_history(hab_id: IdModel, hs: HabitReport = Depends(get_hs)):
+async def get_habit_measure_history(hab_id: UUID, hs: HabitReport = Depends(get_hs)):
     return hs.get_habit_measure_history(hab_id)
 
 @router.get("/yn/resume/{hab_id}", response_model=md.HabitYNResumeReportModel)
-async def get_habit_yn_resume(hab_id: IdModel, hs: HabitReport = Depends(get_hs)):
+async def get_habit_yn_resume(hab_id: UUID, hs: HabitReport = Depends(get_hs)):
     return hs.get_habit_yn_resume(hab_id)
 
 @router.get("/yn/history/{hab_id}", response_model=md.HabitYNHistoryReportModel)
-async def get_habit_yn_history(hab_id: IdModel, hs: HabitReport = Depends(get_hs)):
+async def get_habit_yn_history(hab_id: UUID, hs: HabitReport = Depends(get_hs)):
     return hs.get_habit_yn_history(hab_id)
 
 @router.get("/yn/streaks/{hab_id}", response_model=md.HabitYNStreakReportModel)
-async def get_habit_yn_streaks(hab_id: IdModel, hs: HabitReport = Depends(get_hs)):
+async def get_habit_yn_streaks(hab_id: UUID, hs: HabitReport = Depends(get_hs)):
     return hs.get_habit_yn_streaks(hab_id)
 
 @router.get("/freq_week_day/{hab_id}", response_model=md.HabitFreqWeekDayReportModel)
-async def get_habit_freq_week_day(hab_id: IdModel, hs: HabitReport = Depends(get_hs)):
+async def get_habit_freq_week_day(hab_id: UUID, hs: HabitReport = Depends(get_hs)):
     return hs.get_habit_freq_week_day(hab_id)
