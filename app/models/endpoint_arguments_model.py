113b4db6014109c2786f71409d5e92fc4d4c7760
maherreramu <64550002+maherreramu@users.noreply.github.com>
2023-09-17 23:22:31 -0500

    Update argument regex to pattern on Field()

--- app/models/endpoint_arguments_model.py
+++ app/models/endpoint_arguments_model.py
@@ -1,4 +1,4 @@
 from pydantic import BaseModel, Field
 
 class IdModel(BaseModel):
-    id: str = Field(regex=r'^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$')
+    id: str = Field(pattern=r'^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$')
