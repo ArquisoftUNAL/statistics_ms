874997492575c491010a85a2a61ffdd198332932
maherreramu <64550002+maherreramu@users.noreply.github.com>
2023-09-29 02:38:39 -0500

    Update main app with new services on startup and shutdown

--- app/main.py
+++ app/main.py
@@ -1,11 +1,23 @@
 from fastapi import FastAPI, status, Request
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
+from sqlalchemy.ext.asyncio import create_async_engine
+from beanie import init_beanie
+from motor.motor_asyncio import AsyncIOMotorClient
+import uvicorn
+from app.models.statistics_db_models import ReportDocument
 from app.exceptions.exceptions import AppConnectionError, AppDatabaseError, HabitNotFoundError
 from app.routers import report_router
-import uvicorn
-from sqlalchemy.ext.asyncio import create_async_engine
-from app.common.constants import HABITS_DB_URL
+from app.common.constants import (
+    HABITS_DB_URL, 
+    STATISTICS_DB_URL, 
+    STATISTICS_DB,
+    RABBITMQ_HOST,
+    RABBITMQ_QUEUE,
+    RABBITMQ_USERNAME,
+    RABBITMQ_PASSWORD
+)
+from app.rabbitmq.s_client import RabbitMQClient
 
 app = FastAPI()
 
@@ -27,10 +39,18 @@ app.add_middleware(
 @app.on_event("startup")
 async def startup():
     app.state.engine = await create_async_engine(HABITS_DB_URL)
+    app.state.client = AsyncIOMotorClient(STATISTICS_DB_URL)
+    await init_beanie(database=app.state.client[STATISTICS_DB], document_models=[ReportDocument])
+    app.state.rabbitmq_client = RabbitMQClient(RABBITMQ_HOST, RABBITMQ_QUEUE, RABBITMQ_USERNAME, RABBITMQ_PASSWORD)
+    await app.state.rabbitmq_client.connect()
+    await app.state.rabbitmq_client.start_consuming()
 
 @app.on_event("shutdown")
 async def shutdown():
     await app.state.engine.dispose()
+    await app.state.client.close()
+    await app.state.rabbitmq_client.disconnect()
+
 
 @app.exception_handler(AppConnectionError)
 async def connection_error_handler(request: Request, exc: AppConnectionError):
