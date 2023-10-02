e6ac92c9d404f7be5c8a8c0ef87f81af6d89ca51
maherreramu <64550002+maherreramu@users.noreply.github.com>
2023-10-01 22:54:52 -0500

    Create stattistics repository interface

--- app/repositories/interfaces/statistic_repository_interface.py
+++ app/repositories/interfaces/statistic_repository_interface.py
@@ -6,21 +6,21 @@ from app.models.report_models import HabitMeasureReport, HabitYNReport
 
 class AbstractStatisticRepository(ABC):
     @abstractmethod
-    def get_all_reports(self) -> List[ReportDocument]:
+    async def get_all_reports(self) -> List[ReportDocument]:
         pass
 
     @abstractmethod
-    def get_report_by_id(self, hab_id: UUID) -> Optional[ReportDocument]:
+    async def get_report_by_id(self, hab_id: UUID) -> Optional[ReportDocument]:
         pass
 
     @abstractmethod
-    def create_report(self, report: ReportDocument):
+    async def create_report(self, report: ReportDocument):
         pass
 
     @abstractmethod
-    def update_report(self, hab_id: UUID, new_report: Union[HabitMeasureReport, HabitYNReport]):
+    async def update_report(self, hab_id: UUID, new_report: Union[HabitMeasureReport, HabitYNReport]):
         pass
 
     @abstractmethod
-    def delete_report(self, hab_id: UUID):
+    async def delete_report(self, hab_id: UUID):
         pass
