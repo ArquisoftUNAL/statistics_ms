8d5c59726613a15ad42a1fae5219ffc14cb27787
maherreramu <64550002+maherreramu@users.noreply.github.com>
2023-10-01 22:53:37 -0500

    statistics repository withou exception

--- app/repositories/statistics_repository.py
+++ app/repositories/statistics_repository.py
@@ -4,11 +4,6 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from beanie import init_beanie
 from beanie.odm.operators.update.general import Set, CurrentDate
 from app.models.statistics_db_models import ReportDocument
-from app.exceptions.exceptions import (
-    AppConnectionError,
-    AppDatabaseError,
-    HabitNotFoundError,
-)
 from app.models.report_models import HabitMeasureReport, HabitYNReport
 from .interfaces.statistic_repository_interface import AbstractStatisticRepository
 
@@ -20,49 +15,31 @@ class StatisticsRepository(AbstractStatisticRepository):
         self.collection = self.db["reports"]
 
     async def get_all_reports(self) -> List[ReportDocument]:
-        try:
-            reports = await ReportDocument.find_all().to_list()
-            return reports
-        except Exception as e:
-            raise AppDatabaseError(e)
+        reports = await ReportDocument.find_all().to_list()
+        return reports
 
     async def get_report_by_id(self, hab_id: UUID) -> Optional[ReportDocument]:
-        try:
-            report = await ReportDocument.find_one(ReportDocument.hab_id == hab_id)
-            if report:
-                return report
-            else:
-                raise HabitNotFoundError(f"Habit with id {hab_id} not found")
-        except Exception as e:
-            raise AppDatabaseError(e)
+        report = await ReportDocument.find_one(ReportDocument.hab_id == hab_id)
+        return report
 
     async def create_report(self, report: ReportDocument):
-        try:
-            report.current_date({ReportDocument.created_at, True})
-            report.current_date({ReportDocument.updated_at, True})
-            await report.insert()
-        except Exception as e:
-            raise AppDatabaseError(e)
+        report.current_date({ReportDocument.created_at, True})
+        report.current_date({ReportDocument.updated_at, True})
+        await report.insert()
 
     async def update_report(self, hab_id: UUID, new_report: Union[HabitMeasureReport, HabitYNReport]):
-        try:
-            report = await ReportDocument.find_one(ReportDocument.hab_id == hab_id)
-            await report.update(
-                Set({
+        report = await ReportDocument.find_one(ReportDocument.hab_id == hab_id)
+        await report.update(
+            Set(
+                {
                     ReportDocument.hab_data_count: report.hab_data_count + 1,
-                    ReportDocument.report: new_report
-                    }),
-                CurrentDate({ReportDocument.updated_at: True})
-            )
-        except Exception as e:
-            raise AppDatabaseError(e)
+                    ReportDocument.report: new_report,
+                }
+            ),
+            CurrentDate({ReportDocument.updated_at: True}),
+        )
 
     async def delete_report(self, hab_id: UUID) -> None:
-        try:
-            report = await ReportDocument.find_one(ReportDocument.hab_id == hab_id)
-            if report:
-                await report.delete()
-            else:
-                raise HabitNotFoundError(f"Habit with id {hab_id} not found")
-        except Exception as e:
-            raise AppDatabaseError(e)
+        report = await ReportDocument.find_one(ReportDocument.hab_id == hab_id)
+        if report:
+            await report.delete()
