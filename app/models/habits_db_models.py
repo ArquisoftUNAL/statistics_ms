37f1bdfd4ded5f0beeff1cb45a794f66900159f5
maherreramu <64550002+maherreramu@users.noreply.github.com>
2023-10-01 22:52:09 -0500

    Update habits db model

--- app/models/habits_db_models.py
+++ app/models/habits_db_models.py
@@ -1,10 +1,18 @@
 from pydantic import BaseModel
 from pandas import DataFrame
+from uuid import UUID
+from datetime import date
 
 class Hab(BaseModel):
     hab_is_yn: bool
     hab_freq_type: str
     hab_goal: float
+
+class HabDataCollected(BaseModel):
+    hab_dat_id: UUID
+    hab_dat_amount: float
+    hab_dat_collected_at: date
+    hab_id: UUID
 
 class HabData(BaseModel):
     hab: Hab
