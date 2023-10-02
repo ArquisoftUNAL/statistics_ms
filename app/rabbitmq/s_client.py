00643a0aa4ce3910b69d8d463a8fc503cf987482
maherreramu <64550002+maherreramu@users.noreply.github.com>
2023-10-01 23:26:05 -0500

    Add logic for handling messages

--- app/rabbitmq/s_client.py
+++ app/rabbitmq/s_client.py
@@ -1,15 +1,22 @@
 import pika
 import logging
 from pika.adapters.asyncio_connection import AsyncioConnection
+from app.repositories.habits_repository import HabitRepository
+from app.repositories.statistics_repository import StatisticsRepository
+from app.repositories.interfaces.habits_repository_interface import AbstractHabitReposiory
+from app.repositories.interfaces.statistic_repository_interface import AbstractStatisticRepository
+from app.services.update_report.update_report import UpdateReport
 
 class RabbitMQClient:
-    def __init__(self, host: str, queue: str, username: str, password: str):
+    def __init__(self, host: str, queue: str, username: str, password: str, sql_engine, motor_client):
         self.host = host
         self.queue = queue
         self.username = username
         self.password = password
         self.connection = None
         self.channel = None
+        self.engine = sql_engine
+        self.client = motor_client
 
     async def connect(self):
         credentials = pika.PlainCredentials(self.username, self.password)
@@ -23,7 +30,11 @@ class RabbitMQClient:
     async def callback(self, ch, method, properties, body):
         #Logic for handling messages
         try:
-            "Procesamiento del mensaje"
+            hab_repo: AbstractHabitReposiory = HabitRepository(self.engine)
+            stat_repo: AbstractStatisticRepository = StatisticsRepository(self.client)
+            update_report = UpdateReport(hab_repo, stat_repo)
+            await update_report.update_habit_report(body.decode())
+
         except Exception as e:
             logging.error(f"Error processing message: {e}")
             ch.basic_nack(delivery_tag=method.delivery_tag)
