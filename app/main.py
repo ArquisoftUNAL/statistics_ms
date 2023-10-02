9589fdda7c1223ff328ea903e539f296c3be6597
maherreramu <64550002+maherreramu@users.noreply.github.com>
2023-10-01 23:24:47 -0500

    Update arguments for RabbitMQ instance

--- app/main.py
+++ app/main.py
@@ -6,16 +6,20 @@ from beanie import init_beanie
 from motor.motor_asyncio import AsyncIOMotorClient
 import uvicorn
 from app.models.statistics_db_models import ReportDocument
-from app.exceptions.exceptions import AppConnectionError, AppDatabaseError, HabitNotFoundError
+from app.exceptions.exceptions import (
+    AppConnectionError,
+    AppDatabaseError,
+    HabitNotFoundError,
+)
 from app.routers import report_router
 from app.common.constants import (
-    HABITS_DB_URL, 
-    STATISTICS_DB_URL, 
+    HABITS_DB_URL,
+    STATISTICS_DB_URL,
     STATISTICS_DB,
     RABBITMQ_HOST,
     RABBITMQ_QUEUE,
     RABBITMQ_USERNAME,
-    RABBITMQ_PASSWORD
+    RABBITMQ_PASSWORD,
 )
 from app.rabbitmq.s_client import RabbitMQClient
 
@@ -36,14 +40,25 @@ app.add_middleware(
     allow_headers=["*"],
 )"""
 
+
 @app.on_event("startup")
 async def startup():
     app.state.engine = await create_async_engine(HABITS_DB_URL)
-    app.state.client = AsyncIOMotorClient(STATISTICS_DB_URL)
-    await init_beanie(database=app.state.client[STATISTICS_DB], document_models=[ReportDocument])
-    app.state.rabbitmq_client = RabbitMQClient(RABBITMQ_HOST, RABBITMQ_QUEUE, RABBITMQ_USERNAME, RABBITMQ_PASSWORD)
+    app.state.client = await AsyncIOMotorClient(STATISTICS_DB_URL)
+    await init_beanie(
+        database=app.state.client[STATISTICS_DB], document_models=[ReportDocument]
+    )
+    app.state.rabbitmq_client = RabbitMQClient(
+        RABBITMQ_HOST,
+        RABBITMQ_QUEUE,
+        RABBITMQ_USERNAME,
+        RABBITMQ_PASSWORD,
+        app.state.engine,
+        app.state.client,
+    )
     await app.state.rabbitmq_client.connect()
     await app.state.rabbitmq_client.start_consuming()
+
 
 @app.on_event("shutdown")
 async def shutdown():
@@ -59,12 +74,14 @@ async def connection_error_handler(request: Request, exc: AppConnectionError):
         content={"message": str(exc)},
     )
 
+
 @app.exception_handler(AppDatabaseError)
 async def database_error_handler(request: Request, exc: AppDatabaseError):
     return JSONResponse(
         status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
         content={"message": str(exc)},
     )
+
 
 @app.exception_handler(HabitNotFoundError)
 async def habit_not_found_error_handler(request: Request, exc: HabitNotFoundError):
@@ -73,11 +90,14 @@ async def habit_not_found_error_handler(request: Request, exc: HabitNotFoundErro
         content={"message": str(exc)},
     )
 
+
 app.include_router(report_router.router, prefix="/api/stats", tags=["statistics"])
+
 
 @app.get("/")
 async def health():
     return {"message": "Ok"}
+
 
 if __name__ == "__main__":
     uvicorn.run(app)
