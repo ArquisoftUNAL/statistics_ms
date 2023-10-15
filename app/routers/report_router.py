daf6fa8629e91188af5bb4d75f46eae9d81f18d2
maherreramu <64550002+maherreramu@users.noreply.github.com>
2023-10-15 12:27:52 -0500

    Update endpoints

--- app/routers/report_router.py
+++ app/routers/report_router.py
@@ -1,20 +1,22 @@
 from fastapi import APIRouter, Depends, Request
 from typing import Union
 from app.models.report_models import HabitYNReport, HabitMeasureReport
-from app.services.get_report.get_report import HabitReport
+from app.services.get_report.get_report import GetReport
+from app.services.create_report.create_report import CreateHabitReport
 from app.repositories.habits_repository import HabitRepository
 from app.repositories.statistics_repository import StatisticsRepository
 from app.repositories.interfaces.habits_repository_interface import AbstractHabitReposiory
 from app.repositories.interfaces.statistic_repository_interface import AbstractStatisticRepository
 from uuid import UUID
 
-def get_hr(request: Request):
+def get_gr(request: Request):
     hab_repo: AbstractHabitReposiory = HabitRepository(request.app.state.engine)
     stat_repo: AbstractStatisticRepository = StatisticsRepository(request.app.state.client)
-    return HabitReport(hab_repo, stat_repo)
+    return GetReport(hab_repo, stat_repo)
 
 router = APIRouter()
 
-@router.get("/report/{hab_id}", response_model=Union[HabitMeasureReport, HabitYNReport])
-async def get_habit_report(hab_id: UUID, hr: HabitReport = Depends(get_hr)):
-    return await hr.get_habit_report(hab_id=hab_id)
+@router.get("/{hab_id}", response_model=Union[HabitMeasureReport, HabitYNReport])
+async def get_habit_report(hab_id: UUID, gr: GetReport = Depends(get_gr)):
+    return await gr.get_habit_report(hab_id)
+
