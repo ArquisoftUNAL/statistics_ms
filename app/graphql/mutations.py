56be888a5e47f9726dc90adf66599334056aa82f
maherreramu <64550002+maherreramu@users.noreply.github.com>
2023-10-16 21:44:30 -0500

    Drop reimport

--- app/graphql/mutations.py
+++ app/graphql/mutations.py
@@ -1,17 +1,19 @@
-from pydantic import BaseModel
 from uuid import UUID
 from pydantic import BaseModel
 from datetime import date
+
 
 class Streak(BaseModel):
     date_start: date
     date_end: date
     data: float
 
+
 class UpDateStreak(BaseModel):
     freq_type: str
     hab_id: UUID
     streak: Streak
+
 
 class UpDateStreakMutation(UpDateStreak):
     def __init__(self, variables: UpDateStreak):
@@ -27,4 +29,3 @@ class UpDateStreakMutation(UpDateStreak):
 
     def get_mutation(self):
         return self.query, self.variables
-
