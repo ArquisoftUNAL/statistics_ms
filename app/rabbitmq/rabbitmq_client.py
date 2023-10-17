2e40a4877c55e23ec75e96761a3c15501248c801
maherreramu <64550002+maherreramu@users.noreply.github.com>
2023-10-16 21:45:30 -0500

    rename and complete rabbit client

--- app/rabbitmq/rabbitmq_client.py
+++ app/rabbitmq/rabbitmq_client.py
@@ -5,11 +5,9 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from app.repositories.habits_repository import HabitRepository
 from app.repositories.statistics_repository import StatisticsRepository
 from app.services.update_report.update_report import UpdateReport
-from app.exceptions.exceptions import HabitNotFoundError
 from app.models.queue_msgs import NewHabitData
-from app.graphql.mutations import UpDateStreak, UpDateStreakMutation, Streak
-from app.graphql.graphql_client import GraphQLClient
-
+from app.services.send_streak.send_streak_to_archivements import send_streak_to_archivements
+from app.exceptions.exceptions import GraphqlMutationError
 
 class RabbitMQClient:
     def __init__(
@@ -33,17 +31,24 @@ class RabbitMQClient:
         await self.connection.close()
 
     async def callback(self, message: aio_pika.IncomingMessage):
-        print("On callback...")
         async with message.process():
-            # Lógica para manejar mensajes
             try:
-                raise HabitNotFoundError
-
+                new_hab_data = NewHabitData(**message.body.decode())
+                report_doc = await UpdateReport(self.hab_repo, self.stat_repo).update_habit_report(
+                    new_hab_data
+                )
             except Exception as e:
                 await message.nack(requeue=True)
                 logging.error(f"Error processing message: {e}")
 
             await message.ack()
+
+            try:
+                await send_streak_to_archivements(report_doc)
+            except GraphqlMutationError as e:
+                logging.error(f"Error sending streak to archivements: {e}")
+
+
 
     async def start_consuming(self):
         queue = await self.channel.declare_queue(self.queue, durable=True)
